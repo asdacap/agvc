68f11b71cf16ce566d4149d0891dd0d4b63d3895
Muhammad Amirul Ashraf <asdacap@gmail.com>
2016-04-20 22:30:07 +0800

    Reduced connection timeout and droppable command timeout.

--- server/imports/Settings.js
+++ server/imports/Settings.js
@@ -1,8 +1,8 @@
 
 Settings = {
   tcp_listen_port: 10000,
-  data_sequence_timeout: 10000, // Milisecond before connection is considered dropped
-  dropppable_command_timeout: 10000, // Milisecond before a droppable command is dropped
+  data_sequence_timeout: 5000, // Milisecond before connection is considered dropped
+  dropppable_command_timeout: 5000, // Milisecond before a droppable command is dropped
   ping_interval: 1000, // Milisecond between ping
   client_ping_interval: 1000,
   viewtime_update_interval: 200,
