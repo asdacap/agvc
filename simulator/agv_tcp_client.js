d3e64fef657ee24111a4e36cc371d78e0d35c1c3
amirul <amirul@localhost.localdomain>
2016-04-22 21:07:29 +0800

    Added a simulator.

---
+++ simulator/agv_tcp_client.js
@@ -0,0 +1,47 @@
+
+var settings = {
+  machineId: "ABC",
+  tcpPort: 10000
+}
+
+var net = require('net');
+var split = require('split');
+var prompt = require('prompt');
+
+var socket = net.connect(settings.tcpPort);
+var splitted = socket.pipe(split());
+
+function sendMessage(message){
+  socket.write(message+"\n");
+}
+
+splitted.on("data", function(data){
+  data = data.toString();
+  console.log("Got data "+data);
+  if(data.indexOf("p:") == 0){
+    sendMessage(data);
+  }else if(data.indexOf("cP:") == 0){
+    sendMessage(data);
+  }
+});
+
+sendMessage("machineId:"+settings.machineId);
+
+var rfids = [
+  "04 8e a1 c2 d7 38 81",
+  "04 46 9e c2 d7 38 81",
+  "04 49 9f c2 d7 38 81",
+  "04 81 a5 c2 d7 38 81"
+]
+
+var rfidnum = 0;
+setInterval(function(){
+  sendMessage("rfid:"+rfids[rfidnum]);
+
+  sendMessage("temperature:"+(Math.floor(Math.random()*100)));
+  sendMessage("battery:"+(Math.floor(Math.random()*10)));
+  sendMessage("loopInterval:"+(Math.floor(Math.random()*10)));
+
+  rfidnum++;
+  rfidnum = rfidnum%rfids.length;
+}, 1000);
